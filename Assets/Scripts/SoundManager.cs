35c4f8cc71ad46a68ef9b65dbae148ff26697ed0
kuta20 <ydapple3204@gmail.com>
2023-10-14 23:40:37 +0900

    Add BG

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -24,17 +24,35 @@ public class SoundManager : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name == "MainScene")
+        //인트로 씬이면 전용 배경음악을 재생
+        // scene.name == "인트로 씬이름 넣기 "
+        if (scene.name == "")
         {
-            if (BGSound != null && BGSound.isPlaying) 
-            { BGSound.Stop(); }
-            //BGSound.clip = null;
-            //BGSound.mute = true;
-                        
-            //return;
+            if (BGSound != null && BGSound.clip != BGlist[4])
+            {
+                BGSoundPlay(BGlist[4]);// BGlist[4] 인트로 전용 음악
+                SetVolume(0.2f); 
+            }
+        }
+
+        // 메인화면 씬이면 전용 배경음악을 재생
+        else if (scene.name == "MainScene")
+        {
+            if (BGSound != null && BGSound.clip != BGlist[3])
+            {
+                BGSoundPlay(BGlist[3]);// BGlist[3] 메인화면 전용 음악
+                SetVolume(0.2f);
+            }
         }
         else
         {
+            //스테이지 씬에서는 메인 화면의 음악을 정지하고 씬 번호에 따라 배경음악 클립 인덱스를 계산하고 재생
+            if (BGSound.isPlaying)
+                BGSound.Stop();
+
+
+            //씬 번호에 따라 배경음악 클립 인덱스를 계산하고 재생
+            //BGlist[0~2] 0:초원,1:도시,2:해변
             int currentScene = SceneManager.GetActiveScene().buildIndex -2;
             int clipIndex = ((currentScene - 1) / 3) % BGlist.Length;
             BGSoundPlay(BGlist[clipIndex]);
@@ -50,13 +68,21 @@ public class SoundManager : MonoBehaviour
             BGSound.mute = false;
             BGSound.clip = clip;
             BGSound.loop = true;
-            BGSound.volume = 0.15f;
+            //BGSound.volume = 0.15f;
+            SetVolume(0.15f);
             BGSound.Play();
         }
        
     }
+
     public void BGSoundStop()
     {
         BGSound.Stop();
+    }
+
+    //볼륨 조절
+    public void SetVolume(float volume)
+    {
+        BGSound.volume = volume;
     }
 }
